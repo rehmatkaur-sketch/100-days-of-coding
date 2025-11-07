f4d2b6ae62dfcd9219d4b5fad93b880f7a7c063c
rehmatkaur-sketch <rehmatkaur108@gmail.com>
2025-11-07 14:18:46 +0530

    Update Day01_A_Sum.c

--- Day01_A_Sum.c
+++ Day01_A_Sum.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
-    float num1, num2, sum
+    float num1, num2, sum;
     printf("Enter your first number: ");
     scanf("%f" , &num1);
-    printf("Enter the second number: ")
+    printf("Enter the second number: ");
     scanf("%f" , &num2);
 sum = num1 + num2;
 printf("The sum of %.2f and %.2f is: %.2f\n", num1, num2, sum);
